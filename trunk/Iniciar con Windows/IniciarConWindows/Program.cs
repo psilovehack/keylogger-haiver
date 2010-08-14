590de15ceef3f9f093cac76f6c26c132e23de570
haiver77@gmail.com <haiver77@gmail.com@b158feee-a98c-5411-e867-e78dc5f905f9>
2010-08-14 04:12:19 +0000

    Commit Inicial IniciarConWindows

---
+++ trunk/Iniciar con Windows/IniciarConWindows/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+
+
+namespace IniciarConWindows
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 formulario = new Form1();
+            Application.Run(formulario);
+            formulario.Dispose();
+            Application.Exit();
+        }
+    }
+
+
+   
+
+  
+}
---
+++ trunk/Iniciar con Windows/IniciarConWindows/class_apagado.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Utilidades
+{
+    public class ApagarEquipo
+    {
+        string argumento = null;
+        DateTime tmp;
+        public ApagarEquipo(string argumento, DateTime tmp)
+        {
+            this.argumento = argumento;
+            this.tmp = tmp;
+        }
+        public void Shut_Down()
+        {
+            try
+            {
+                while (true)
+                {
+                    if (tmp.ToLongTimeString() == DateTime.Now.ToLongTimeString())
+                    {
+                        Process proceso = new Process();
+                        proceso.StartInfo.UseShellExecute = false;
+                        proceso.StartInfo.RedirectStandardOutput = true;
+                        proceso.StartInfo.FileName = "shutdown.exe";
+                        proceso.StartInfo.Arguments = this.argumento;
+                        proceso.Start();
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+
+}
