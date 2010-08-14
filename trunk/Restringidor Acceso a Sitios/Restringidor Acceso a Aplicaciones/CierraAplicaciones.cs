56c335cb942039aae48ecf6a0cb684000b536606
haiver77@gmail.com <haiver77@gmail.com@b158feee-a98c-5411-e867-e78dc5f905f9>
2010-08-14 04:13:19 +0000

    Commit Inicial Restringidor Acceso a Aplicaciones

---
+++ trunk/Restringidor Acceso a Sitios/Restringidor Acceso a Aplicaciones/CierraAplicaciones.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Restringidor_Acceso_a_Aplicaciones
+{
+    static class CierraAplicaciones
+    {
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg, IntPtr wParam, IntPtr lParam);
+
+        const int WM_COMMAND = 0x111;
+        const int MIN_ALL = 419;
+        const int MIN_ALL_UNDO = 416;
+
+        public static void cerrarNavegadoresAbiertos()
+        {
+            {
+                
+                System.Diagnostics.Process[] procesos;
+
+                procesos = System.Diagnostics.Process.GetProcessesByName("firefox");
+                foreach (System.Diagnostics.Process proceso in procesos)
+                    proceso.Kill();
+
+                procesos = System.Diagnostics.Process.GetProcessesByName("chrome");
+                foreach (System.Diagnostics.Process proceso in procesos)
+                    proceso.Kill();
+
+                procesos = System.Diagnostics.Process.GetProcessesByName("msnmsgr");
+                foreach (System.Diagnostics.Process proceso in procesos)
+                    proceso.Kill();
+
+                //IntPtr lHwnd = FindWindow("Shell_TrayWnd", null);
+                //SendMessage(lHwnd, WM_COMMAND, (IntPtr)MIN_ALL, IntPtr.Zero);
+                //System.Threading.Thread.Sleep(2000);
+                //SendMessage(lHwnd, WM_COMMAND, (IntPtr)MIN_ALL_UNDO, IntPtr.Zero);
+            }
+
+        }
+    }
+}
