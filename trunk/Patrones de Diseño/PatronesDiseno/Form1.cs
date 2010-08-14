e37c8ef9a1c567b43698621cea719e562a7e15f8
haiver77@gmail.com <haiver77@gmail.com@b158feee-a98c-5411-e867-e78dc5f905f9>
2010-08-14 04:12:59 +0000

    Commit Inicial PatronesDiseno

---
+++ trunk/Patrones de Diseño/PatronesDiseno/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using Builder;
+using Adapter;
+using Decorator;
+using Command;
+using State;
+using Visitor;
+using Observer;
+
+
+namespace wfaPatronesDiseno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ComputadoraBuilder c;
+
+            Produccion p = new Produccion();
+            
+            c = new PersonalJuegosBuilder();
+            p.Construir(c);
+            c.Computadora.MostrarPartes();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Compuesto c = new Compuesto("Cualquiera");
+            c.Mostrar();
+
+            Compuesto agua = new CompuestoCompleto("Agua");
+            agua.Mostrar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Libro objLibro = new Libro("Nicolas");
+            objLibro.Mostrar();
+
+            Prestable objPrestable = new Prestable(objLibro);
+            objPrestable.PrestarItem("Cliente 1");
+            objPrestable.PrestarItem("Cliente 2");
+
+            objPrestable.Mostrar();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Usuario objUsuario = new Usuario();
+            objUsuario.Calcular('+', 10);
+            objUsuario.Calcular('-', 5);
+            objUsuario.Calcular('*', 2);
+
+            objUsuario.Deshacer(1);
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Cuenta objCuenta = new Cuenta("Nicolas");
+            objCuenta.Depositar(100);
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Employees empleados = new Employees();
+            empleados.Attach(new Director());
+
+            empleados.Accept(new VisitorIngreso());
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Despensa d = new Despensa("Azucar",5);
+            d.Attach(new Iversor("Nicolas"));
+            d.Attach(new Iversor("Juan"));
+
+            d.Precio = 6;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FactoriaContinente fc = new FactoriaAmerica();
+            MundoAnimal a = new MundoAnimal(fc);
+            a.CadenaAlimenticia();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Composite.Compuesto c = new Composite.Compuesto("Figuras");
+            c.Agregar(new Composite.Primitiva("Auto"));
+            c.Agregar(new Composite.Primitiva("Moto"));
+
+            c.Mostrar(1);
+            
+        }
+    }
+}
---
+++ trunk/Patrones de Diseño/PatronesDiseno/Visitor/Empleados.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Visitor
+{
+    class Employees
+    {
+        private ArrayList employees = new ArrayList();
+
+        public void Attach(Empleado employee)
+        {
+            employees.Add(employee);
+        }
+
+        public void Detach(Empleado employee)
+        {
+            employees.Remove(employee);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach (Empleado e in employees)
+            {
+                e.Aceptar(visitor);
+            }
+            //Notificar Pantalla
+        }
+    }
+}
