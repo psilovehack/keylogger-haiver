c903c6ba722a86541577ab09e08d65c5e02bcc47
haiver77@gmail.com <haiver77@gmail.com@b158feee-a98c-5411-e867-e78dc5f905f9>
2010-08-14 04:12:41 +0000

    Commit Inicial KeyLogger

---
+++ trunk/Keylog/KeyLoggerLocal/frmKeyLogger.Designer.cs
@@ -0,0 +1,24 @@
+namespace KeyLoggerLocal
+{
+    partial class frmKeyLogger
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {}
+        #endregion
+
+    }
+}
+
