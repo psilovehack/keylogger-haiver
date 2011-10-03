7cf0a633740d61fbd6d8c153904594e2c79e7a6c
haiver77 <haiver77@gmail.com>
2011-10-03 23:06:32 +0000

    ObtenerColorPixel SimulacionEventoClick

---
+++ AppSubastas/AppSubastas/Supervisor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Collections;
+
+namespace AppSubastas
+{
+
+    
+
+    class Supervisor
+    {
+        Thread hilo;
+        private ThreadStart Supervisar;
+
+        public Supervisor()
+        {
+            hilo = new Thread(new ThreadStart(SupervisarValores));
+        }
+
+        public void SupervisarValores()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                //hacer click para generar archivo con la lectura de pantalla
+
+
+
+                //abrir archivo para interpretar lectura
+                StreamReader objReader = new StreamReader("c:\\test.txt");
+                string sLine = "";
+
+                
+                string primeraLinea = objReader.ReadLine();
+                string segundaLinea = objReader.ReadLine();
+
+
+
+                //objReader.Close();
+
+                //foreach (string sOutput in arrText)
+                //    Console.WriteLine(sOutput);
+                //Console.ReadLine();
+
+
+                //tomar decicion si aplicar a la oferta
+            }
+        }
+    }
+}
---
+++ ObtenerColorPixel/ObtenerColorPixel/Win32APICall.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace ObtenerColorPixel
+{
+    class Win32APICall
+    {
+        private Bitmap myBitmap;
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteDC")]
+        public static extern IntPtr DeleteDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        public static extern IntPtr DeleteObject(IntPtr hObject);
+
+        [DllImport("gdi32.dll", EntryPoint = "BitBlt")]
+        public static extern bool BitBlt(IntPtr hdcDest, int nXDest,
+            int nYDest, int nWidth, int nHeight, IntPtr hdcSrc,
+            int nXSrc, int nYSrc, int dwRop);
+
+        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleBitmap")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hdc,
+            int nWidth, int nHeight);
+
+        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleDC")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll", EntryPoint = "SelectObject")]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobjBmp);
+
+        [DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll", EntryPoint = "GetDC")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetSystemMetrics")]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "ReleaseDC")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        public static Bitmap GetDesktop()
+        {
+            int screenX;
+            int screenY;
+            IntPtr hBmp;
+            IntPtr hdcScreen = GetDC(GetDesktopWindow());
+            IntPtr hdcCompatible = CreateCompatibleDC(hdcScreen);
+
+            screenX = GetSystemMetrics(0);
+            screenY = GetSystemMetrics(1);
+            hBmp = CreateCompatibleBitmap(hdcScreen, screenX, screenY);
+
+            if (hBmp != IntPtr.Zero)
+            {
+                IntPtr hOldBmp = (IntPtr)SelectObject(hdcCompatible, hBmp);
+                BitBlt(hdcCompatible, 0, 0, screenX, screenY, hdcScreen, 0, 0, 13369376);
+
+                SelectObject(hdcCompatible, hOldBmp);
+                DeleteDC(hdcCompatible);
+                ReleaseDC(GetDesktopWindow(), hdcScreen);
+
+                Bitmap bmp = System.Drawing.Image.FromHbitmap(hBmp);
+
+                DeleteObject(hBmp);
+                GC.Collect();
+
+                return bmp;
+            }
+
+            return null;
+        }
+
+
+        private void label1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            myBitmap = Win32APICall.GetDesktop();
+        }
+
+
+        //private void label1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        //{
+        //    Color myColor = myBitmap.GetPixel(MousePosition.X, MousePosition.Y);
+        //    label1.BackColor = myColor;
+        //}
+
+        public Color GetPixelColor(int x, int y)
+        {
+           myBitmap = Win32APICall.GetDesktop();
+           return myBitmap.GetPixel(x, y);
+        }
+
+        public Point encontrarColor(string colorHexadecimal)
+        {
+            for (int i = 0; i < myBitmap.Height; i++)
+            {
+                for (int j = 0; j < myBitmap.Width; j++)
+                {
+                    if (GetPixelColor(i, j).Name == colorHexadecimal)
+                        return new Point(i, j);
+
+                }
+            }
+            return new Point();
+        }
+    }
+}
